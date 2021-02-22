f4eb80c37dd53cde25c01df0de770f685c28a9cf
Nexch <mauricio.t.1999@gmail.com>
2021-02-22 13:02:31 -0800

    fix desing and add rspecs

--- spec/follow_spec.rb
+++ spec/follow_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'friends', type: :feature do
+RSpec.describe 'follow', type: :feature do
   scenario 'accept1' do
     visit new_user_registration_path
     fill_in 'Name', with: 'Nata'
@@ -9,21 +9,8 @@ RSpec.describe 'friends', type: :feature do
     fill_in 'Password confirmation', with: 'natanata'
     click_on 'Sign up'
 
-    visit '/tweets/new'
-    fill_in 'tweet_tweet', with: 'some text \n'
-    expect(page).to have_text('Tweet something')
-  end
-
-  scenario 'accept' do
-    visit new_user_registration_path
-    fill_in 'Name', with: 'Nata'
-    fill_in 'Email', with: 'nata@nata'
-    fill_in 'Password', with: 'natanata'
-    fill_in 'Password confirmation', with: 'natanata'
-    click_on 'Sign up'
-
-    visit '/'
-    expect(page).to have_text('Tweet')
+    click_on 'User page'
+    expect(page).to have_text('Tweets')
   end
 
   scenario 'no requests' do
