1abec0ec8488689395e4266a81705f07d9088e2c
Nexch <mauricio.t.1999@gmail.com>
2021-02-24 08:58:29 -0800

    allow spaces

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   acts_as_votable
   belongs_to :user
   validates :tweet, presence: true
-  validates :tweet, format: { with: /\A[a-zA-Z]+\z/,
+  validates :tweet, format: { with: /\A[a-zA-Z0-9 ]+\z/,
                               message: 'only allows letters' }
   validates :tweet, length: { maximum: 100 }
 end
