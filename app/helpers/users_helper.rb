3c075f8c4d79b5b8abd59489958cc0f552c01662
Nexch <mauricio.t.1999@gmail.com>
2021-02-24 12:08:50 -0800

    fix follow problem

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,19 +1,16 @@
 module UsersHelper
-  def user_follow
+  def user_follow(lol)
     return unless current_user.id.to_i != params[:id].to_i
-
-    if Follow.where(user_id: current_user, friend_id: params[:id]) == []
-      form_with model: @friendship, url: follows_path do |f|
-        content_tag(:div, class: 'field') do
-          f.hidden_field :friend_id, value: params[:id]
+      if Follow.where(user_id: current_user, friend_id: params[:id]) == []
+        form_with model: @friendship, url: follows_path do |f|
+          f.hidden_field :friend_id, value: lol
           f.submit 'Follow', type: :image, src: 'https://cdn2.iconfinder.com/data/icons/everything-but-the-kitchen-sink-2/100/common-06-512.png', id: 'lol', alt: 'Take'
         end
-      end
 
-    else
-      content_tag(:h5) do
-        link_to 'Unfollow', follow_path, method: :delete
+      else
+        content_tag(:h5) do
+          link_to 'Unfollow', follow_path, method: :delete
+        end
       end
-    end
   end
 end
