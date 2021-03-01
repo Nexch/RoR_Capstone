b51f6aa463b4b446eb7deefe0dea2b3acb1ed295
Nexch <mauricio.t.1999@gmail.com>
2021-03-01 12:42:28 -0800

    delete edit and destroy controllers and views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -29,9 +29,6 @@ class TweetsController < ApplicationController
     @tweet = current_user.tweets.build
   end
 
-  # GET /tweets/1/edit
-  def edit; end
-
   # POST /tweets or /tweets.json
   def create
     @tweet = current_user.tweets.build(tweet_params)
@@ -44,28 +41,6 @@ class TweetsController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /tweets/1 or /tweets/1.json
-  def update
-    respond_to do |format|
-      if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tweet }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /tweets/1 or /tweets/1.json
-  def destroy
-    @tweet.destroy
-    respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
