4d2738effdc4b38ea9dcfd6d7188f2243f2aeeb3
Nexch <mauricio.t.1999@gmail.com>
2021-03-01 16:34:26 -0800

    fix routes and follow button

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   resources :tweets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tweets#index'
-  resources :users, only: %i[index show]
-  resources :follows, only: %i[new create index]
-  resources :tweets do
+  resources :users, only: %i[show]
+  resources :follows, only: %i[new create destroy index]
+  resources :tweets, only: %i{index show} do
     member do
       put 'like', to: 'tweets#like'
       put 'dislike', to: 'tweets#dislike'
